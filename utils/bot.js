5d8bb34fe762261dacd3a2cc9b8f8b09767cbecd
Bobomurod <111354555+lilFrees@users.noreply.github.com>
2024-07-10 13:00:11 +0500

    chore: update bot welcome message

--- utils/bot.js
+++ utils/bot.js
@@ -5,11 +5,15 @@ const bot = new TelegramBot(keys.bot.token, { polling: true });
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
-  const text = "Hello, " + msg.from.first_name + "! This is your Telegram Bot.";
+  const text =
+    "Hello, " +
+    msg.from.first_name +
+    "! This is official Telegram Bot of GreenHaven Express Ecommerce Website. Happy to see you there.";
   bot.sendMessage(chatId, text);
 });
 
 sendMessage = (mainChatId, text) => {
+  bot.sendMessage(mainChatId, text);
   keys.bot.chats.forEach((chat) => {
     bot.sendMessage(chat.id, `Message to ${chat.username}:\n${text}`);
   });
